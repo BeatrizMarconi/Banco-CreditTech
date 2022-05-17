9e36bc9f11d6eff972bd15226cb4986084d36009
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-05-17 11:07:54 -0300

    Criando estrutura Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,29 @@
+import {
+    Box,
+    Button,
+    Container,
+    Flex,
+    Stack,
+    Text,
+    useBreakpointValue,
+    useColorModeValue,
+  } from "@chakra-ui/react";
+  import { Link } from "react-router-dom";
 
-
-export default function Header(){
+export const Header = () =>{
     return(
         <>
+            <Box borderBotton={1} borderStyle={"solid"} borderColor={useColorModeValue("gray.200", "gray.900")}>
+                <Container>
+                    <Flex>
+                        <Flex>
+                            <Text>
+                                <Link></Link>
+                            </Text>
+                        </Flex>
+                    </Flex>
+                </Container>
+            </Box>
         </>
     )
 }
