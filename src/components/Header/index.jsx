88fe883c47d1c368a99f41c20746e5d868388bff
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-05-17 15:12:24 -0300

    Criando Auth

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
     Box,
     Button,
@@ -7,20 +8,80 @@ import {
     Text,
     useBreakpointValue,
     useColorModeValue,
-  } from "@chakra-ui/react";
-  import { Link } from "react-router-dom";
+    Image,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.jpg";
+import { isLogged } from "../../services";
 
-export const Header = () =>{
-    return(
+export default function Header() {
+
+    const [userIsLogged] = isLogged();
+    return (
         <>
-            <Box borderBotton={1} borderStyle={"solid"} borderColor={useColorModeValue("gray.200", "gray.900")}>
-                <Container>
-                    <Flex>
-                        <Flex>
-                            <Text>
-                                <Link></Link>
+            <Box
+                borderBottom={1}
+                borderStyle={"solid"}
+                borderColor={useColorModeValue("gray.200", "gray.900")}
+            >
+                <Container maxW={"8xl"}>
+                    <Flex
+                        bg={useColorModeValue("white", "gray.800")}
+                        color={useColorModeValue("gray.600", "white")}
+                        minH={"60px"}
+                        py={{ base: 2 }}
+                        px={{ base: 4 }}
+                        align={"center"}
+                    >
+                        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+                            <Text
+                                textAlign={useBreakpointValue({ base: "center", md: "left" })}
+                                fontFamily={"heading"}
+                                color={useColorModeValue("gray.800", "white")}
+                            >
+                                <Link to="/"><Image src={logo} boxSize='80px' /></Link>
                             </Text>
                         </Flex>
+
+                        {userIsLogged !== '' ?
+                            <Stack
+                                flex={{ base: 1, md: 0 }}
+                                justify={"flex-end"}
+                                direction={"row"}
+                                spacing={6}>
+
+                                <Button fontSize={"sm"}
+                                    fontWeight={600}
+                                    color={"white"}
+                                    bg={"pink.400"}
+                                    href={"#"}
+                                    _hover={{
+                                        bg: "pink.300",
+                                    }}>
+
+                                </Button>
+                            </Stack>
+                            :
+                            <Stack
+                                flex={{ base: 1, md: 0 }}
+                                justify={"flex-end"}
+                                direction={"row"}
+                                spacing={6}>
+
+                                <Button
+                                    as={"a"}
+                                    padding={0}
+                                    fontSize={"sm"}
+                                    fontWeight={400}
+                                    variant={"link"}
+                                    href={"#"}>
+
+                                    <Link style={{padding: "12px 15px"}} to="/login">Acessar</Link>
+
+                                </Button>
+
+                            </Stack>
+                        }
                     </Flex>
                 </Container>
             </Box>
--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,2 +1,25 @@
+export const auth = ({email, password}) => {
 
+    const userEmail = 'teste@teste.com';
+    const userPassword = 1234;
+    let token = '';
 
+    //if para gerar um token aleatorio e guardar no local storage e retornar true
+    if(userEmail === email && userPassword === password){
+        token.match.random().toString(36);
+        window.localStorage.setItem('token', JSON.stringify(token));
+        return true;
+    }
+
+    //se não entrar no if, guarda no local storage o token vazio e retorna false
+    window.localStorage.setItem('token', JSON.stringify(token));
+    return false;
+};
+
+export const isLogged = () => {
+
+    // guarda na cost o token do local storage formatado e retorna o token
+    const userIsLogged = JSON.parse(window.localStorage.getItem('token'));
+
+    return [userIsLogged];
+};
