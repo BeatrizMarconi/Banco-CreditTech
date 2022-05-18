787f47c73508542512cf60714dc430592d5a7718
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-05-18 12:42:59 -0300

    criando rota operações

--- src/pages/Operations/index.jsx
+++ src/pages/Operations/index.jsx
@@ -12,7 +12,7 @@ export default function Operations() {
         if (userIsLogged === "") {
             navigate(`/login`);
         }
-    }, []);
+    },);
     return (
         <>
             <Header/>
